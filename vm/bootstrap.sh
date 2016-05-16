898a0d6b43a118eb9d739da30c4eee4d6d49ad1a
Greg Elin <greg@fotonotes.net>
2016-05-16 15:38:20 -0400

    Fix Vagrantfile

--- vm/bootstrap.sh
+++ vm/bootstrap.sh
@@ -2,5 +2,21 @@
 
 apt-get update
 
-# install python
-apt-get install python-dev python-pip -q -y
+# install pip for python 3
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
+
+# remove get-pip.py file since we installed pip
+rm get-pip.py
+
+# install git
+sudo apt-get install git -y
+
+# install repo
+# git clone https://github.com/GovReady/compliancekbs.git
+
+# install a few needed libraries
+sudo apt-get install sqlite3 htmldoc poppler-utils -y
+
+# install requirements for our kbs flask app
+sudo pip3 install -r requirements.txt
